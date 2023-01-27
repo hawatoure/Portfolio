f6789a497363108169568a4d8fa6cedaaae87d16
hawatoure <hawa_toure@outlook.fr>
2023-01-27 12:00:24 +0100

    first commit

---
+++ assets/inc/footerFront.php
@@ -0,0 +1,20 @@
+  
+  
+  
+  
+  <!-- footer -->
+   <div class="container">
+  <footer class="bg-dark text-light py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Accueil</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Envoyer un message</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Me contacter</a></li>
+    </ul>
+    <p class="text-center text-muted">&copy; 2022 Company, Inc</p>
+  </footer>
+</div>
+
+    <script src="assets/js/"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    // 
+    //require('controllers/.)
+    define("PAGE_TITLE", "Accueil"); // To define a constance, use define(): first param = name of de const, second param= title name
+?>
+
+<?php include('assets/inc/headFront.php');?>
+
+
+<?php include('assets/inc/headerFront.php');?>
+
+<main>
+
+
+
+</main>
+
+<?php include('assets/inc/footerFront.php');?>
